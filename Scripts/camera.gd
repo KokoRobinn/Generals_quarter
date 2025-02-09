e397b3a3aa5b38c8ba4ef6fccf23e3f03630ef97
Adam Magnusson <admag2001@gmail.com>
2025-02-09 02:38:25 +0100

    Heightfields now depend on a list of layers which get random noise offsets and have variable noise zoom and amplification

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -7,8 +7,10 @@ var rot_y = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	RenderingServer.set_debug_generate_wireframes(true)
 	print("Setting input to true in camera\n")
 	set_process_input(true)
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -25,9 +27,9 @@ func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_D):
 		translate_object_local(Vector3(speed, 0, 0))
 	if Input.is_key_pressed(KEY_SPACE):
-		translate_object_local(Vector3(0, speed, 0))
+		translate(Vector3(0, speed, 0))
 	if Input.is_key_pressed(KEY_SHIFT):
-		translate_object_local(Vector3(0, -speed, 0))
+		translate(Vector3(0, -speed, 0))
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().quit()
 
@@ -39,3 +41,11 @@ func _input(event):
 		transform.basis = Basis() # reset rotation
 		rotate_object_local(Vector3(0, -1, 0), rot_x) # first rotate in Y
 		rotate_object_local(Vector3(-1, 0, 0), rot_y) # then rotate in X
+	if Input.is_key_pressed(KEY_X):
+		print(transform.origin)
+	if Input.is_key_pressed(KEY_0):
+		transform = Transform3D.IDENTITY
+		
+	if event is InputEventKey and Input.is_key_pressed(KEY_V):
+		var vp = get_viewport()
+		vp.debug_draw = (vp.debug_draw + 1) % 5
--- Scripts/generate_ground.gd
+++ Scripts/generate_ground.gd
@@ -1,9 +1,32 @@
 extends MeshInstance3D
 
+class NoiseLayer:
+	var zoom := 1.0
+	var amp := 1.0
+	
+	func _init(z, a) -> void:
+		zoom = z
+		amp = a
+
+const MULTI_THREADING := true
+const UNIT_PER_QUAD := 10.0
+@export var seed := 1543515.74356 
+@export var noise_layers := [
+	NoiseLayer.new(1. / 7.0, 3.0),
+	NoiseLayer.new(1. / 2.0, 0.4),
+	NoiseLayer.new(1. / 1.0, 0.3)
+]
 @export var mult := 10.0
-@export var regenerate := false
-@export var heights := preload("res://.godot/imported/heights.png-7ec082f11699a676eeebe61cba7126e6.ctex") as CompressedTexture2D
+@export var map_size := Vector2i(1024, 1024)
+@export var regenerate := false :
+	set(value):
+		regen()
+var regenerating := false
+@export var heights := preload("res://Textures/heights.png")
 @export var tex := preload("res://.godot/imported/heights.png-7ec082f11699a676eeebe61cba7126e6.ctex")
+#@export var noise_layers := 3
+var num_layers := len(noise_layers)
+var noise_imgs: Array
 var heights_image: Image
 var surface_array = []
 var verts = PackedVector3Array()
@@ -12,19 +35,49 @@ var normals = PackedVector3Array()
 var indices = PackedInt32Array()
 var w: int
 var h: int
-var num_threads: int
+var num_threads := 1
+var noise := NoiseTexture2D.new()
+var time_start: int
 
+# Fetching heights from noise is probably qiute innefficient atm.
+# Should pre-fetch images from each layer at their designated rng offsets 
 func thread_generate(thread) -> void:
-	print("Assigned lines %d to %d to thread %d" % [h / num_threads * thread, h / num_threads * (thread + 1), thread])
-	for y in range(h / num_threads * thread, h / num_threads * (thread + 1)):
-		print("starting line %d in thread %d\n" % [y, thread])
+	var end
+	if thread == num_threads - 1:
+		end = h
+	else:
+		end = h / num_threads * (thread + 1)
+	print("Assigned lines %d to %d to thread %d" % [h / num_threads * thread, end, thread])
+	var rng = RandomNumberGenerator.new()
+	rng.set_seed(seed)
+	for y in range(h / num_threads * thread, end):
+		#print("starting line %d in thread %d\n" % [y, thread])
 		for x in range(w):
-			var vert = Vector3(x, heights_image.get_pixel(x, y).r * mult, y)
+			var local_seed = rng.randf_range(0.0, 1024.0)
+			var noise_sum = 0
+			for noise_height in range(num_layers).map(func(idx): 
+					var vec = Vector2(local_seed + x, local_seed + y) * noise_layers[idx].zoom
+					rng.set_seed(seed)
+					return noise.noise.get_noise_2dv(vec) * noise_layers[idx].amp):
+				noise_sum += noise_height * mult
+			local_seed = seed
+			var vert = Vector3(float(x) * UNIT_PER_QUAD, noise_sum * mult, float(y) * UNIT_PER_QUAD)
 			verts[y * w + x] = vert
 			normals[y * w + x] = vert.normalized()
 			uvs[y * w + x] = Vector2(float(x) / (w - 1), float(y) / (h - 1))
-
+	
 func generate() -> void:
+	w = map_size.x / UNIT_PER_QUAD
+	h = map_size.y / UNIT_PER_QUAD
+	print("Heightmap size is %d x %d\n" % [w, h])
+	print("Resizing buffers\n")
+	verts.resize(w * h)
+	uvs.resize(w * h)
+	normals.resize(w * h)
+	indices = PackedInt32Array()
+	indices.resize(w * h * 6)
+	if MULTI_THREADING:
+		num_threads = min(OS.get_processor_count(), max(h, w))
 	print("Creating threads\n")
 	var task_id = WorkerThreadPool.add_group_task(thread_generate, num_threads)
 	WorkerThreadPool.wait_for_group_task_completion(task_id)
@@ -43,6 +96,7 @@ func generate() -> void:
 	surface_array[Mesh.ARRAY_NORMAL] = normals
 	surface_array[Mesh.ARRAY_INDEX] = indices
 	
+	mesh = ArrayMesh.new()
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
 	
 	var material = StandardMaterial3D.new()
@@ -51,23 +105,24 @@ func generate() -> void:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	time_start = Time.get_ticks_msec()
 	print("Resizing surface array\n")
 	surface_array.resize(Mesh.ARRAY_MAX)
-	print("Importing heightmap\n")
-	heights_image = heights.get_image()
-	w = heights.get_width()
-	h = heights.get_height()
-	print("Heightmap size is %d x %d\n" % [w, h])
-	print("Resizing buffers\n")
-	verts.resize(w * h)
-	uvs.resize(w * h)
-	normals.resize(w * h)
-	indices.resize(w * h * 6)
-	num_threads = min(OS.get_processor_count(), h)
+	noise.noise = FastNoiseLite.new()
+	await noise.changed
 	print("Starting mesh generation\n")
 	generate()
 	
+func regen() -> void:
+	print("regenerating\n")
+	if time_start + 2000 < Time.get_ticks_msec() and not regenerating:
+		regenerating = true
+		print("entering generate\n")
+		generate()
+		regenerate = false
+		regenerating = false
+		notify_property_list_changed()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if regenerate:
-		generate()
+	pass
